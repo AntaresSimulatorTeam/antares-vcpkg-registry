4bbb58bd7c17e48a18c7274eb66507e5b8831106
Jason Marechal <jason.marechal+github@gmail.com>
2024-09-25 14:53:57 +0200

    Fork or-tools-rte

--- ports/or-tools-rte/portfile.cmake
+++ ports/or-tools-rte/portfile.cmake
@@ -6,17 +6,30 @@ vcpkg_from_github(
         HEAD_REF master
 )
 
-set(VERSION 9.10-rte1.2)
+set(VERSION branch)
 vcpkg_download_distfile(ARCHIVE
-        URLS "https://github.com/JasonMarechal25/or-tools-rte/archive/refs/tags/v${VERSION}.tar.gz"
+        URLS "https://github.com/JasonMarechal25/or-tools-rte/archive/refs/heads/feature/option_sirius.zip"
         FILENAME "or-tools-v${VERSION}.tar.gz"
-        SHA512 7861b1958e43998f4b80447594f2580716880f0eada4114ee47be48efbf448688b3c0b597ded0269525b05e3ac4d74cc5ccff4146b608b8089c7f17f0c5a0666
+        SHA512 d28f16e95156b28a511d8454b3bf3035e4517e980d58c3e0a250fe7a62c8e5f26de036aa4be9ca903626c338aec7006afe815bfda329f1958bf14fcc4665a42b
 )
 
-vcpkg_execute_required_process(COMMAND tar xzvf "${ARCHIVE}" --strip-components=1 -C "${SOURCE_PATH}" --exclude "CMakeLists.txt"
+#vcpkg_execute_required_process(COMMAND tar xzvf "${ARCHIVE}" --strip-components=1 -C "${SOURCE_PATH}" --exclude "CMakeLists.txt"
+#        WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
+#        LOGNAME "ortools-untar"
+#)
+
+vcpkg_find_acquire_program(7Z)
+
+vcpkg_execute_required_process(COMMAND ${7Z} x "${ARCHIVE}" -o "${SOURCE_PATH}/or-tools-rte"
         WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
         LOGNAME "ortools-untar"
 )
+
+vcpkg_execute_required_process(COMMAND find "${SOURCE_PATH}/or-tools-rte/" -maxdepth 1 ! -name "CMakeLists.txt" -exec mv {} "${SOURCE_PATH}/" \;
+        WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
+        LOGNAME "ortools-untar"
+)
+
 
 vcpkg_find_acquire_program(PYTHON3)
 get_filename_component(PYTHON3_DIR "${PYTHON3}" DIRECTORY)
