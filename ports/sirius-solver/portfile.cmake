dde4a0dd41d4e76a16a80e6d1c1bc50c1ac06e40
Jason Marechal <jason.marechal+github@gmail.com>
2025-02-11 17:22:48 +0100

    Update hash

--- ports/sirius-solver/portfile.cmake
+++ ports/sirius-solver/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO "rte-france/sirius-solver"
         REF "antares-integration-v1.7"
-        SHA512 0c9fc5134db3a3ad42a15aca98ecead93afdc6f5
+        SHA512 39c5935a3b96d58fa35e7b20ef0bc65d78fca0f6ba9c01824e121c5d309d27b9c77b4fc4976c28f18e835596fb598355aee21bd524541bd01a26846973fd8ca4
         HEAD_REF main
 )
 
