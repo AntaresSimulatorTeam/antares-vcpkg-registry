2466962fe2833892cd6044907d5a202f442213bb
Jason Marechal <jason.marechal+github@gmail.com>
2025-03-10 13:31:00 +0100

    Test port

--- ports/or-tools-rte/cmake_patches/sirius.cmake
+++ ports/or-tools-rte/cmake_patches/sirius.cmake
@@ -1,6 +1,7 @@
 include(FetchContent)
 find_package(sirius_solver QUIET)
 if (NOT sirius_solver_FOUND AND BUILD_SIRIUS)
+    error("SIRIUS not found, but BUILD_SIRIUS is enabled")
     message("SIRIUS not found, fetching it from github")
     FetchContent_Declare(sirius_solver
             GIT_REPOSITORY https://github.com/rte-france/sirius-solver
