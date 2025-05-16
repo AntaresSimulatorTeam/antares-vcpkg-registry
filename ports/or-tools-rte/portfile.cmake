4855cbadebb291749cf201818c0955991318795a
Jason Maréchal <45510813+JasonMarechal25@users.noreply.github.com>
2025-05-16 12:36:04 +0200

    Or tools rte (#4)

--- ports/or-tools-rte/portfile.cmake
+++ ports/or-tools-rte/portfile.cmake
@@ -1,18 +1,35 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO "google/or-tools"
-        REF "b39974ba209d9f5215bdc363bb8ad9625443d618" #9.10
-        SHA512 183440e6a2d821a643664bd8ca5e72a0c75adfa5cab76fa2647a3b04f0006ac86761147ae1a7e72edb6c90b626c11b3bc5ea3d5135aa654e3be7b6028ded08a2
+        REF "b8e881fbde473a9e33e0dac475e498559eb0459d" #9.12
+        SHA512 ac0b5c9870a2b887610540266a5260c6756052ed5a212b0ad78447e8e9651e1fb3b6abe271b7758d4913397000c083b9e4e56297c259c58b8b40562463c63f78
         HEAD_REF master
+)
+
+set(VERSION 9.12-rte1.1)
+vcpkg_download_distfile(ARCHIVE
+        URLS "https://github.com/rte-france/or-tools-rte/archive/refs/tags/v${VERSION}.tar.gz"
+        FILENAME "or-tools-v${VERSION}.tar.gz"
+        SHA512 64194a126c4b470415c3ca04bd9e8412cdb2b42e891b8584c97dcddca1a5af934ca08f6781f83f10bebba5dc7e58f9948d0656079a4818fc82eb6f0225dd26aa
+)
+
+vcpkg_execute_required_process(COMMAND tar xzvf "${ARCHIVE}" --strip-components=1 -C "${SOURCE_PATH}" --exclude "CMakeLists.txt"
+        WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
+        LOGNAME "ortools-untar"
 )
 
 vcpkg_find_acquire_program(PYTHON3)
 get_filename_component(PYTHON3_DIR "${PYTHON3}" DIRECTORY)
 vcpkg_add_to_path("${PYTHON3_DIR}")
 
-vcpkg_execute_process(COMMAND ${PYTHON3} patch.py"
+vcpkg_execute_required_process(COMMAND ${PYTHON3} patch.py
         WORKING_DIRECTORY "${SOURCE_PATH}"
-        LOGNAME "pip-install"
+        LOGNAME "patch-install"
+)
+
+vcpkg_execute_required_process(COMMAND ls
+        WORKING_DIRECTORY "${SOURCE_PATH}"
+        LOGNAME "patch-install"
 )
 
 vcpkg_cmake_configure(
@@ -28,7 +45,6 @@ vcpkg_cmake_configure(
         -DBUILD_ZLIB=OFF
         -DUSE_HIGHS=OFF
         -DBUILD_TESTING=OFF
-        -DBUILD_SIRIUS=OFF
         -DUSE_SIRIUS=ON
 )
 
